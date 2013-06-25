ad03f9757210aebbe92e8a8198501353bb255b99
Peter Lappo <peter.lappo@smr.co.uk>
2013-06-25 23:16:59 +0100

    allow other user

--- testgradle.sh
+++ testgradle.sh
@@ -15,104 +15,117 @@ source shunit_assert.sh
 # T6 install named projects with an installVersion
 #    install project, install does not exist (OK), install fails, install with deploy version fails
 
+if [ $# -eq 2 ] ; then
+  target=$1
+  thome=$2
+  dhome="$2/deploy_$1"
+  ahome="$2/archive_$1"
+else
+  target=local
+  thome=${HOME}
+  dhome="${HOME}/deploy_local"
+  ahome="${HOME}/archive_local"
+fi
+echo "Running tests for target=$target userhomedir=$thome deploydir=$dhome archivedir=$ahome"
+
 function before() {
   rm -rf ~/deploy_local ~/archive_local/*
 }
 
 function T1() {
-	before
-	gradle -P destination=local -P buildNumber=1 clean
-	assertStat $? "T1 clean"
-	assertNotExists build
-	
-	gradle -P destination=local -P buildNumber=1 build
-	assertStat $? "T1 build"
-	assertFileExists build/distributions/testenv-1.0.1.tgz "T1 build"
-	
-	gradle -P destination=local -P buildNumber=1 archive
-	assertStat $? "T1 archive"
-	assertFileExists ~/archive_local/testenv-1.0.1.tgz "T1 archive"
-	
-	gradle -P destination=local -P buildNumber=1 deploy
-	assertStat $? "T1 deploy"
-	assertDirExists ~/deploy_local/testenv-1.0.1 "T1 deploy"
-	
-	gradle -P destination=local -P buildNumber=1 install
-	assertStat $? "T1 install"
+  before
+  gradle -P destination=local -P buildNumber=1 clean
+  assertStat $? "T1 clean"
+  assertNotExists build
+  
+  gradle -P destination=local -P buildNumber=1 build
+  assertStat $? "T1 build"
+  assertFileExists build/distributions/testenv-1.0.1.tgz "T1 build"
+  
+  gradle -P destination=local -P buildNumber=1 archive
+  assertStat $? "T1 archive"
+  assertFileExists ~/archive_local/testenv-1.0.1.tgz "T1 archive"
+  
+  gradle -P destination=local -P buildNumber=1 deploy
+  assertStat $? "T1 deploy"
+  assertDirExists ~/deploy_local/testenv-1.0.1 "T1 deploy"
+  
+  gradle -P destination=local -P buildNumber=1 install
+  assertStat $? "T1 install"
 
   gradle -P destination=local -P buildNumber=1 rollback
   assertStat $? "T1 rollback"
 }
 
 function T2() {
-	before
-	
-	gradle --settings-file settings.gradle.sub -P destination=local -P buildNumber=1 clean
-	assertStat $? "T2 clean"
-	assertNotExists build
-	
-	gradle --settings-file settings.gradle.sub -P destination=local -P buildNumber=1 build
-	assertStat $? "T2 build"
-	assertFileExists build/distributions/testenv_root-1.0.1.tgz "T2 build testenv_root"
-	assertFileExists sub1/build/distributions/sub1-1.0.1.tgz "T2 build sub1"
-	assertFileExists sub2/build/distributions/sub2-1.0.1.tgz "T2 build sub2"
-	
-	gradle --settings-file settings.gradle.sub -P destination=local -P buildNumber=1 archive
-	assertStat $? "T2 archive"
-	assertFileExists ~/archive_local/testenv_root-1.0.1.tgz "T2 archive testenv_root"
-	assertFileExists ~/archive_local/sub1-1.0.1.tgz "T2 archive sub1"
-	assertFileExists ~/archive_local/sub2-1.0.1.tgz "T2 archive sub2"
-	
-	gradle --settings-file settings.gradle.sub -P destination=local -P buildNumber=1 deploy
-	assertStat $? "T2 deploy"
-	assertDirExists ~/deploy_local/testenv_root-1.0.1 "T2 deploy testenv_root"
-	assertDirExists ~/deploy_local/sub1-1.0.1 "T2 deploy sub1"
-	assertDirExists ~/deploy_local/sub2-1.0.1 "T2 deploy sub2"
-	
+  before
+  
+  gradle --settings-file settings.gradle.sub -P destination=local -P buildNumber=1 clean
+  assertStat $? "T2 clean"
+  assertNotExists build
+  
+  gradle --settings-file settings.gradle.sub -P destination=local -P buildNumber=1 build
+  assertStat $? "T2 build"
+  assertFileExists build/distributions/testenv_root-1.0.1.tgz "T2 build testenv_root"
+  assertFileExists sub1/build/distributions/sub1-1.0.1.tgz "T2 build sub1"
+  assertFileExists sub2/build/distributions/sub2-1.0.1.tgz "T2 build sub2"
+  
+  gradle --settings-file settings.gradle.sub -P destination=local -P buildNumber=1 archive
+  assertStat $? "T2 archive"
+  assertFileExists ~/archive_local/testenv_root-1.0.1.tgz "T2 archive testenv_root"
+  assertFileExists ~/archive_local/sub1-1.0.1.tgz "T2 archive sub1"
+  assertFileExists ~/archive_local/sub2-1.0.1.tgz "T2 archive sub2"
+  
+  gradle --settings-file settings.gradle.sub -P destination=local -P buildNumber=1 deploy
+  assertStat $? "T2 deploy"
+  assertDirExists ~/deploy_local/testenv_root-1.0.1 "T2 deploy testenv_root"
+  assertDirExists ~/deploy_local/sub1-1.0.1 "T2 deploy sub1"
+  assertDirExists ~/deploy_local/sub2-1.0.1 "T2 deploy sub2"
+  
 #gradle --settings-file settings.gradle.sub -P destination=local -P buildNumber=1 install
 #assertStat $? "T2 install"
 }
 
 function T3() {
-	before
-	
-	gradle --settings-file settings.gradle.sub -P destination=local clean
-	assertStat $? "T3 clean"
-	assertNotExists build
-	
-	gradle --settings-file settings.gradle.sub -P destination=local build
-	assertStat $? "T3 build"
-	assertFileExists build/distributions/testenv_root-1.0.SNAPSHOT.tgz "T3 build testenv_root"
-	assertFileExists sub1/build/distributions/sub1-1.0.SNAPSHOT.tgz "T3 build sub1"
-	assertFileExists sub2/build/distributions/sub2-1.0.SNAPSHOT.tgz "T3 build sub2"
-	
-	gradle --settings-file settings.gradle.sub -P destination=local archive
-	assertStatFailed $? "T3 archive"
-	assertNotExists ~/archive_local/testenv_root-1.0.SNAPSHOT.tgz "T3 archive testenv_root"
-	assertNotExists ~/archive_local/sub1-1.0.SNAPSHOT.tgz "T3 archive sub1"
-	assertNotExists ~/archive_local/sub2-1.0.SNAPSHOT.tgz "T3 archive sub2"
-	
-	gradle --settings-file settings.gradle.sub -P destination=local -P buildNumber=1 deploy
-	assertStatFailed $? "T3 deploy"
-	assertNotExists ~/deploy_local/testenv_root-1.0.SNAPSHOT "T3 deploy testenv_root"
-	assertNotExists ~/deploy_local/sub1-1.0.SNAPSHOT "T3 deploy sub1"
-	assertNotExists ~/deploy_local/sub2-1.0.SNAPSHOT "T3 deploy sub2"
+  before
+  
+  gradle --settings-file settings.gradle.sub -P destination=local clean
+  assertStat $? "T3 clean"
+  assertNotExists build
+  
+  gradle --settings-file settings.gradle.sub -P destination=local build
+  assertStat $? "T3 build"
+  assertFileExists build/distributions/testenv_root-1.0.SNAPSHOT.tgz "T3 build testenv_root"
+  assertFileExists sub1/build/distributions/sub1-1.0.SNAPSHOT.tgz "T3 build sub1"
+  assertFileExists sub2/build/distributions/sub2-1.0.SNAPSHOT.tgz "T3 build sub2"
+  
+  gradle --settings-file settings.gradle.sub -P destination=local archive
+  assertStatFailed $? "T3 archive"
+  assertNotExists ~/archive_local/testenv_root-1.0.SNAPSHOT.tgz "T3 archive testenv_root"
+  assertNotExists ~/archive_local/sub1-1.0.SNAPSHOT.tgz "T3 archive sub1"
+  assertNotExists ~/archive_local/sub2-1.0.SNAPSHOT.tgz "T3 archive sub2"
+  
+  gradle --settings-file settings.gradle.sub -P destination=local -P buildNumber=1 deploy
+  assertStatFailed $? "T3 deploy"
+  assertNotExists ~/deploy_local/testenv_root-1.0.SNAPSHOT "T3 deploy testenv_root"
+  assertNotExists ~/deploy_local/sub1-1.0.SNAPSHOT "T3 deploy sub1"
+  assertNotExists ~/deploy_local/sub2-1.0.SNAPSHOT "T3 deploy sub2"
 }
 
 function T4() {
-	before
-	
-	gradle --settings-file settings.gradle.sub.fail -P destination=local -P buildNumber=1 clean build archive
-	assertStat $? "T4 build"
-	
-	gradle -P destination=local -P buildNumber=1 -P archiveRoot=testenv_root deploy
-	assertStat $? "T4 deploy testenv_root"
-	assertDirExists ~/deploy_local/testenv_root-1.0.1 "deploy testenv_root"
-	assertExists ~/deploy_local/testenv_root "dir testenv_root"
-	
-	gradle -P destination=local -P buildNumber=1 -P archiveRoot=testenv_root install
-	assertStat $? "T4 install testenv_root"
-	
+  before
+  
+  gradle --settings-file settings.gradle.sub.fail -P destination=local -P buildNumber=1 clean build archive
+  assertStat $? "T4 build"
+  
+  gradle -P destination=local -P buildNumber=1 -P archiveRoot=testenv_root deploy
+  assertStat $? "T4 deploy testenv_root"
+  assertDirExists ~/deploy_local/testenv_root-1.0.1 "deploy testenv_root"
+  assertExists ~/deploy_local/testenv_root "dir testenv_root"
+  
+  gradle -P destination=local -P buildNumber=1 -P archiveRoot=testenv_root install
+  assertStat $? "T4 install testenv_root"
+  
   gradle -P destination=local -P buildNumber=1 -P archiveRoot=testenv_root rollback
   assertStat $? "T4 rollback testenv_root"
   
@@ -123,18 +136,18 @@ function T4() {
   assertStatFailed $? "T4 deploy does not exist"
   
   gradle -P destination=local -P buildNumber=1 -P archiveRoot=sub1 deploy install
-	assertStat $? "T4 install not exist (OK)"
-	assertDirExists ~/deploy_local/sub1-1.0.1 "T4 deploy sub1"
-	assertExists ~/deploy_local/sub1 "T4 dir sub1"
+  assertStat $? "T4 install not exist (OK)"
+  assertDirExists ~/deploy_local/sub1-1.0.1 "T4 deploy sub1"
+  assertExists ~/deploy_local/sub1 "T4 dir sub1"
 
-	gradle -P destination=local -P buildNumber=1 -P archiveRoot=sub1 rollback
+  gradle -P destination=local -P buildNumber=1 -P archiveRoot=sub1 rollback
   assertStat $? "T4 rollback not exist (OK)"
-			
-	gradle -P destination=local -P buildNumber=1 -P archiveRoot=subfail deploy install
-	assertStatFailed $? "T4 install fails"
-	assertDirExists ~/deploy_local/subfail-1.0.1 "T4 deploy subfail"
-	assertExists ~/deploy_local/subfail "T4 dir subfail"
-	assertExists ~/deploy_local/subfail/install/install-1.0.sh  "T4 install exists subfail"
+      
+  gradle -P destination=local -P buildNumber=1 -P archiveRoot=subfail deploy install
+  assertStatFailed $? "T4 install fails"
+  assertDirExists ~/deploy_local/subfail-1.0.1 "T4 deploy subfail"
+  assertExists ~/deploy_local/subfail "T4 dir subfail"
+  assertExists ~/deploy_local/subfail/install/install-1.0.sh  "T4 install exists subfail"
 
   gradle -P destination=local -P buildNumber=1 -P archiveRoot=subfail rollback
   assertStatFailed $? "T4 rollback fails"
